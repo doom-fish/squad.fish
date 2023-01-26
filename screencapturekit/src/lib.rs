bac425bb6fb6fa4cf7ae9a35e73ce1d74fd558b0
1313 <mail@perjohansson.net>
2023-01-26 13:08:25 +0100

    working test

--- screencapturekit/src/lib.rs
+++ screencapturekit/src/lib.rs
@@ -10,13 +10,7 @@ pub struct SCShareableContent {}
 impl SCShareableContent {
     pub fn get_with_completion_handler(completion_handler: fn(Result<SCShareableContent, Error>)) {
         let completion_handler_block = ConcreteBlock::new(|_sc: Id<Object>, _error: Id<Object>| {});
-        let _: Id<Object> = unsafe {
-            let c = class!(SCShareableContent);
-            msg_send![
-                c,
-                getShareableContentWithCompletionHandler: completion_handler_block
-            ]
-        };
+        let _: Id<Object> = unsafe { msg_send![class!(SCShareableContent), alloc] };
         completion_handler(Ok(SCShareableContent {}))
     }
 }
@@ -26,6 +20,6 @@ mod tests {
     use super::*;
     #[test]
     fn test_get_with_completion_handler() {
-        assert!(true, "TRUE");
+        SCShareableContent::get_with_completion_handler(|_: Result<SCShareableContent, Error>| {});
     }
 }

31fa344b97f8ef4ac46ab03a17857453eb5b888a
1313 <mail@perjohansson.net>
2023-01-31 21:29:30 +0100

    working prototpe of get SCSharableContent

--- screencapturekit/src/lib.rs
+++ screencapturekit/src/lib.rs
@@ -1,15 +1,35 @@
+use block::ConcreteBlock;
 use objc::{runtime::Object, *};
 use objc_id::Id;
+use std::sync::mpsc::{channel, RecvError};
 
-pub enum Error {
-    SharableContentError,
-}
+type id = *mut Object;
 
-pub struct SCShareableContent {}
+struct NSError(id);
+
+pub struct SCShareableContent(Id<Object>);
 
 impl SCShareableContent {
-    pub fn get_with_compeletion_handler() {
-        let _: Id<Object> = unsafe { msg_send![class!(SCShareableContent), alloc] };
+    pub fn get() -> Result<Self, RecvError> {
+        let (tx, rx) = channel();
+
+        let handler = ConcreteBlock::new(|sc_ptr: id, _: NSError| {
+            let sc_id: Id<Object> = unsafe { Id::from_ptr(sc_ptr) };
+            tx.send(SCShareableContent(sc_id)).expect("Should work!");
+        });
+
+        #[allow(clippy::redundant_clone)]
+        let raw_handler = &*handler.clone();
+        let _: () = unsafe {
+            msg_send![
+                class!(SCShareableContent),
+                getShareableContentWithCompletionHandler: raw_handler
+            ]
+        };
+        rx.recv()
+    }
+    pub fn windows(&self) -> id {
+        unsafe { msg_send!(self.0, windows) }
     }
 }
 
@@ -18,6 +38,7 @@ mod tests {
     use super::*;
     #[test]
     fn test_get_with_completion_handler() {
-        SCShareableContent::get_with_compeletion_handler();
+       let sc = SCShareableContent::get().unwrap();
+        sc.windows();
     }
 }

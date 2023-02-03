6c38c84a48f62935e1cdcc15d3bb919f2dc851cc
1313 <mail@perjohansson.net>
2023-02-03 20:54:47 +0100

    pointer cleanup

--- screencapturekit/src/lib.rs
+++ screencapturekit/src/lib.rs
@@ -8,13 +8,10 @@ use objc_foundation::{INSArray, INSObject, INSString, NSArray, NSString};
 use objc_id::Id;
 use std::sync::mpsc::{channel, RecvError};
 
-type IdPtr = *mut Object;
 type WindowID = u32;
 type DisplayID = u32;
 
-struct NSError(IdPtr);
-
-pub struct SCShareableContent(Id<Object>);
+struct NSError;
 
 pub struct Size {
     width: f64,
@@ -55,35 +52,28 @@ pub struct SCDisplay {
 struct RawSCRunnableApplication;
 unsafe impl Message for RawSCRunnableApplication {}
 
+macro_rules! get_string {
+    // The `expr` designator is used for expressions.
+    ($obj:ident, $name: ident) => {{
+        let string_ptr: *const NSString = msg_send![$obj, $name];
+        if string_ptr.is_null() {
+            None
+        } else {
+            Some((*string_ptr).as_str())
+        }
+    }};
+}
+
 impl RawSCRunnableApplication {
-    
     fn get_process_id(&self) -> isize {
-        unsafe { msg_send![&*self, processID] }
+        unsafe { msg_send![self, processID] }
     }
 
     fn get_application_name(&self) -> Option<&str> {
-        unsafe {
-            let obj = &*(self as *const _ as *const Object);
-            let str_id: IdPtr = msg_send![obj, applicationName];
-            if str_id.is_null() {
-                None
-            } else {
-                let str = str_id as *const NSString;
-                Some((*str).as_str())
-            }
-        }
+        unsafe { get_string!(self, applicationName) }
     }
     fn get_bundle_identifier(&self) -> Option<&str> {
-        unsafe {
-            let obj = &*(self as *const _ as *const Object);
-            let str_id: IdPtr = msg_send![obj, bundleIdentifier];
-            if str_id.is_null() {
-                None
-            } else {
-                let str = str_id as *const NSString;
-                Some((*str).as_str())
-            }
-        }
+        unsafe { get_string!(self, bundleIdentifier) }
     }
 }
 
@@ -91,27 +81,18 @@ struct RawSCWindow;
 unsafe impl Message for RawSCWindow {}
 
 impl RawSCWindow {
-    fn get_owning_application(&self) -> &RawSCRunnableApplication {
-        unsafe { msg_send![&*self, owningApplication] }
+    fn get_owning_application(&self) -> Id<RawSCRunnableApplication> {
+        unsafe { Id::from_ptr(msg_send![self, owningApplication]) }
     }
     fn get_window_layer(&self) -> u32 {
-        unsafe { msg_send![&*self, windowLayer] }
+        unsafe { msg_send![self, windowLayer] }
     }
     fn get_window_id(&self) -> WindowID {
-        unsafe { msg_send![&*self, windowID] }
+        unsafe { msg_send![self, windowID] }
     }
 
     fn get_title(&self) -> Option<&str> {
-        unsafe {
-            let obj = &*(self as *const _ as *const Object);
-            let str_id: IdPtr = msg_send![obj, title];
-            if str_id.is_null() {
-                None
-            } else {
-                let str = str_id as *const NSString;
-                Some((*str).as_str())
-            }
-        }
+        unsafe { get_string!(self, title) }
     }
 }
 
@@ -121,17 +102,16 @@ impl INSObject for RawSCWindow {
             .expect("Missing SCWindow class, check that the binary is linked with ScreenCaptureKit")
     }
 }
-
-impl SCShareableContent {
-    pub fn get() -> Result<Self, RecvError> {
+struct RawSCShareableContent;
+unsafe impl Message for RawSCShareableContent {}
+impl RawSCShareableContent {
+    pub fn get() -> Result<Id<Self>, RecvError> {
         let (tx, rx) = channel();
 
-        let handler = ConcreteBlock::new(move |sc_ptr: IdPtr, _: NSError| {
-            let sc_id: Id<Object> = unsafe { Id::from_ptr(sc_ptr) };
-            tx.send(SCShareableContent(sc_id)).expect("Should work!");
-        });
-
         unsafe {
+            let handler = ConcreteBlock::new(move |sc: *mut RawSCShareableContent, _: NSError| {
+                tx.send(Id::from_ptr(sc)).expect("Should work!");
+            });
             let _: () = msg_send![
                 class!(SCShareableContent),
                 getShareableContentWithCompletionHandler: &*handler.copy()
@@ -142,9 +122,14 @@ impl SCShareableContent {
     }
     pub fn windows(&self) -> Vec<SCWindow> {
         let win_array_ptr: Id<NSArray<RawSCWindow>> =
-            unsafe { Id::from_ptr(msg_send!(self.0, windows)) };
+            unsafe { Id::from_ptr(msg_send!(self, windows)) };
         for (_, w) in win_array_ptr.object_enumerator().enumerate() {
-           println!("www {}", w.get_owning_application().get_application_name().unwrap_or("NONE"));
+            println!(
+                "www {}",
+                w.get_owning_application()
+                    .get_application_name()
+                    .unwrap_or("NONE")
+            );
         }
         vec![]
     }
@@ -155,7 +140,7 @@ mod tests {
     use super::*;
     #[test]
     fn test_get() {
-        let sc = SCShareableContent::get().unwrap();
+        let sc = RawSCShareableContent::get().unwrap();
         sc.windows();
     }
 }

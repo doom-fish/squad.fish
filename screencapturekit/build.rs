6a17ae546b0300af1018a7472b3825c7fcb51387
1313 <mail@perjohansson.net>
2023-03-03 11:53:39 +0100

    fix build script

--- screencapturekit/build.rs
+++ screencapturekit/build.rs
@@ -1,18 +1,29 @@
-use std::{env, process::Command};
 use copy_to_output::copy_to_output;
+use std::{env, process::Command};
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", $($tokens)*)
+    }
+}
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let profile = env::var("PROFILE").unwrap();
+    let target_dir = format!("{}/target/{}", env::var("CARGO_MANIFEST_DIR").unwrap(), profile);
 
-
-     // Re-runs script if any files in res are changed  
-    println!("cargo:rerun-if-changed=protocolfix/ProtocolFix.h");  
-    println!("cargo:rerun-if-changed=protocolfix/ProtocolFix.m");  
+    // Re-runs script if any files in res are changed
+    println!("cargo:rerun-if-changed=protocolfix/ProtocolFix.h");
+    println!("cargo:rerun-if-changed=protocolfix/ProtocolFix.m");
 
     Command::new("make")
         .args(["-C", "protocolfix"])
         .status()
         .unwrap();
 
-    copy_to_output("protocolfix/ProtocolFix.framework", &env::var("PROFILE").unwrap()).expect("Could not copy");  
+    p!(target_dir);
+    copy_to_output("protocolfix/ProtocolFix.framework", profile.as_str()).expect("Could not copy");
+
+    println!("cargo:rustc-link-search=framework={}", target_dir);
+    println!("cargo:rustc-link-lib=framework=ProtocolFix");
     println!("cargo:rustc-link-lib=framework=ScreenCaptureKit");
     println!("cargo:rustc-link-lib=framework=CoreFoundation");
     println!("cargo:rustc-link-lib=framework=CoreMedia");
